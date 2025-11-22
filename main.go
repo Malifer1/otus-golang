940075c9d58af68f9d6d9837b9832b24b9f5cf30
Malifer1 <62737321+Malifer1@users.noreply.github.com>
2025-11-22 18:40:00 +0300

    chessboard

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func create_chessboard(size int) string {
+	var board strings.Builder
+	for i := 0; i < size; i++ {
+		for j := 0; j < size; j++ {
+			if (i+j)%2 == 0 {
+				board.WriteString(" ")
+			} else {
+				board.WriteString("#")
+			}
+		}
+		if i < size-1 {
+			board.WriteString("\n")
+		}
+	}
+
+	return board.String()
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+	size := 8
+	chessBoard := create_chessboard(size)
+	fmt.Println("Chessobard with size: ", size)
+	fmt.Println(chessBoard)
 }
