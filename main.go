c9be23b96f710af9dbba819bf24b4e757fdc33c2
Malifer1 <62737321+Malifer1@users.noreply.github.com>
2025-11-22 19:40:20 +0300

    added user input for chessboard creation and some value checks

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strings"
+    "strconv"
 )
 
 func create_chessboard(size int) string {
@@ -24,8 +25,19 @@ func create_chessboard(size int) string {
 }
 
 func main() {
-	size := 8
-	chessBoard := create_chessboard(size)
-	fmt.Println("Chessobard with size: ", size)
-	fmt.Println(chessBoard)
+    var stdin string
+    fmt.Print("Enter size of chessboard: ")
+	fmt.Scanln(&stdin)
+	size, err:= strconv.Atoi(strings.TrimSpace(stdin))
+	if err != nil {
+		fmt.Println("An error:\n", err, "\noccured after entering number")
+		return
+	}
+	if size <= 0 {
+		fmt.Println("Size of chessboard must be a positive value!")
+		return
+	}
+	board := create_chessboard(size)
+    fmt.Println("Chessobard with size: ", size)
+	fmt.Println(board)
 }
